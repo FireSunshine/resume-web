641426ede8f3d9cf8a8e679cbaf572d33887758d
sunshine <2898205320@qq.com>
2022-07-18 11:44:59 +0800

    init-cli

--- app/renderer/app.tsx
+++ app/renderer/app.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import Title from './title';
+import img from './6.jpeg';
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
         <Route path="/">
           <div>可视化简历平台</div>
           <div>这是 Electron + React </div>
+          <Title text="配置完成" styles={{ background: 'red' }} />
+          <img src={img} />
         </Route>
       </Switch>
     </Router>
